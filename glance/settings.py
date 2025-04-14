8d4566bffb27b34c34c26f0b2f9ad4d1040b61ec
porkush <parasmittal2018@gmail.com>
2025-04-14 10:50:20 +0530

    local backup

--- glance/settings.py
+++ glance/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'student.apps.StudentConfig',
     "administration.apps.AdministrationConfig",
     'volunteer.apps.VolunteerConfig',
+    'dbbackup'
     
     'import_export',
     "user_visit"
