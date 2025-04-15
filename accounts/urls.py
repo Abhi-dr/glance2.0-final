fcf3d14fa9b72ca363aa73081fe4d0fa228f7344
porkush <parasmittal2018@gmail.com>
2025-04-15 16:33:56 +0530

    error fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,10 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from .api_views import StudentsDataTablesView
 
 urlpatterns = [
-    path('login', views.login, name="login"),
-    path('register', views.register, name="register"),
-    path('logout', views.logout, name="logout"),
-    path("Terms-and-conditions", views.tnc, name="tnc"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout, name="logout"),
+    path('terms-and-conditions/', views.tnc, name="tnc"),
 
     path('check_username_availability/', views.check_username_availability, name='check_username_availability'),
     path('check_mobile_availability/', views.check_mobile_availability, name='check_mobile_availability'),
